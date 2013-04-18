89f1113c7bab853dcee5f0eeeb671cc058d2ae76
tkatchev <tkatchev@gmail.com>
2013-04-18 09:05:43 +0400

    .

--- counters.h
+++ counters.h
@@ -12,6 +12,18 @@ struct Counts {
 
     std::map< unsigned int, std::map<std::string, unsigned int> > data;
 
+    /*
+    void print() {
+        std::cout << "====" << std::endl;
+        for (const auto& i : data) {
+            for (const auto& j : i.second) {
+                std::cout << i.first << " '" << j.first << "' " << j.second << std::endl;
+            }
+        }
+        std::cout << "-==-" << std::endl;
+    }
+    */
+
     void init(const std::string& tag, unsigned int level, unsigned int maxc) {
 
         if (maxc > 0) {
@@ -54,10 +66,11 @@ struct Counts {
                 --ntag;
             }
 
-            unsigned int takecount = take(level, i->first, n);
+            std::string tag = i->first;
+            unsigned int takecount = take(level, tag, n);
 
             n -= takecount;
-            ret[i->first] += takecount;
+            ret[tag] += takecount;
         }
 
         return ret;
