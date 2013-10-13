eb8d7ead08c28c430e9b5c6ecd7fab17f01f5c97
tkatchev <tkatchev@gmail.com>
2013-10-13 15:36:36 +0400

    .

--- platform/linux/makedist.sh
+++ platform/linux/makedist.sh
@@ -10,28 +10,50 @@ cp $BUILDDIR/incavead $BUILDDIR/highscore $BUILDDIR/victory $BUILDDIR/printmap d
 cp `ldd dist/bin/incavead | grep -o '\W/[^ ]*'` dist/bin
 cp $DATADIR/*.cfg dist
 
-cat << "EOF" > dist/incavead_putty
+cat << "EOF" > dist/incavead_server
 #!/bin/bash
 SCRIPT_PATH=$(dirname $(readlink -f $0))
-$SCRIPT_PATH/bin/ld-*.so.2 --library-path $SCRIPT_PATH/bin $SCRIPT_PATH/bin/incavead --singleplayer &
-putty telnet://0.0.0.0:20020
+$SCRIPT_PATH/bin/ld-*.so.2 --library-path $SCRIPT_PATH/bin $SCRIPT_PATH/bin/incavead $*
 EOF
 
-cat << "EOF" > dist/incavead_telnet
+cat << "EOF" > dist/incavead
 #!/bin/bash
-SCRIPT_PATH=$(dirname $(readlink -f $0))
-$SCRIPT_PATH/bin/ld-*.so.2 --library-path $SCRIPT_PATH/bin $SCRIPT_PATH/bin/incavead --singleplayer &
-telnet 0.0.0.0 20020
+
+case $2 in 
+ascii) ./incavead_server --singleplayer & ;;
+unicode) ./incavead_server --unicode --singleplayer & ;;
+*) echo "Usage: ./incavead {putty|telnet} {unicode|ascii}. See README."; exit 1 ;;
+esac
+
+case $1 in
+putty) putty telnet://0.0.0.0:20020 ;;
+telnet) telnet 0.0.0.0 20020 ;;
+*) echo "Usage: ./incavead {putty|telnet} {unicode|ascii}. See README."; exit 1 ;;
 EOF
 
-chmod +x dist/incavead_putty
-chmod +x dist/incavead_telnet
+chmod +x dist/incavead_server
+chmod +x dist/incavead
 
 cat << "EOF" > dist/README
 
-To start the game run either 'incavead_putty' or 'incavead_telnet'.
+There are four ways to play the game:
 
-'incavead_putty' launches Putty as a telnet client, while 'incavead_telnet' uses the standard console 'telnet' command.
+ 1. With Putty client and ASCII graphics:
+    
+    $ ./incavead putty ascii
+
+ 2. With Putty client and Unicode graphics:
+
+    $ ./incavead putty unicode
+
+ 3. With the console telnet client and ASCII graphics:
+
+    $ ./incavead telnet ascii
+
+ 4. With the console telnet client and Unicode graphics:
+
+    $ ./incavead telnet unicode
+
 
 (Putty is usually faster and more configurable as a telnet client, but may require some fiddling with settings.)
 
--- platform/windows/makedist.sh
+++ platform/windows/makedist.sh
@@ -13,11 +13,19 @@ $MINGW/bin/objdump -x dist/bin/incavead.exe | grep 'DLL Name: ' | grep -o ': .*'
 
 cp $DATADIR/*.cfg dist
 
-cat << "EOF" > dist/incavead.bat
+cat << "EOF" > dist/incavead_ascii.bat
 @ECHO OFF
 start "" /b bin\incavead.exe --singleplayer
 start putty -load game telnet://127.0.0.1:20020
 EOF
+
+cat << "EOF" > dist/incavead_unicode.bat
+@ECHO OFF
+start "" /b bin\incavead.exe --unicode --singleplayer
+start putty -load game telnet://127.0.0.1:20020
+EOF
+
+cp dist/incavead_unicode.bat dist/incavead.bat
 
 #wget -Pdist 'http://the.earth.li/~sgtatham/putty/latest/x86/puttytel.exe'
 
@@ -29,7 +37,10 @@ cp game.session dist/putty/sessions
 
 cat << "EOF" > dist/README.txt
 
-Run 'incavead.bat' to launch the game.
+Run 'incavead_ascii.bat' to launch the game with ASCII graphics.
+Run 'incavead_unicode.bat' to launch the game with Unicode graphics.
+
+  P.S. 'incavead.bat' is the same thing as 'incavead_unicode.bat'.
 
 (Note: the game does not require Internet access. Everything is purely offline, Putty is used only as a convenient text-mode rendering client.)
 EOF
