0f56fb84ef75f31da8bd9b55576b15f5b963ec6d
tkatchev <tkatchev@gmail.com>
2014-01-04 15:44:26 +0400

    .

--- platform/linux/makedist.sh
+++ platform/linux/makedist.sh
@@ -3,6 +3,15 @@
 BUILDDIR=$1
 DATADIR=$2
 
+ARCH=$3
+
+if [[ -z $ARCH ]]
+then
+  ARCH=x86_64
+fi
+
+rm -rf incavead*
+
 mkdir dist
 mkdir dist/bin
 
@@ -25,68 +34,52 @@ CLIENT=$1
 
 if [[ -z $@ ]]
 then
-  MODE="unicode"
-  CLIENT="telnet"
+  MODE="tiles"
+  CLIENT="tiletel"
 fi
 
 case $MODE in 
 ascii) ./.incavead_server --singleplayer ;;
 unicode) ./.incavead_server --unicode --singleplayer ;;
 tiles) ./.incavead_server --tiles --singleplayer ;;
-*) echo "Usage: ./incavead {putty|telnet} {tiles|unicode|ascii}. See README."; exit 1 ;;
+*) echo "Usage: ./incavead {tiletel|telnet} {tiles|unicode|ascii}. See README."; exit 1 ;;
 esac
 
 case $CLIENT in
-putty) putty telnet://0.0.0.0:20020 ;;
+tiletel) cd tiletel; ./tiletel localhost 20020 ;;
 telnet) telnet 0.0.0.0 20020; echo -e '\033[0m' ;;
-*) echo "Usage: ./incavead {putty|telnet} {tiles|unicode|ascii}. See README."; exit 1 ;;
+*) echo "Usage: ./incavead {tiletel|telnet} {tiles|unicode|ascii}. See README."; exit 1 ;;
 esac
 EOF
 
 chmod +x dist/.incavead_server
 chmod +x dist/incavead
 
+tar -C dist -xvzf tiletel-$ARCH.tar.gz
+
 cat << "EOF" > dist/README
 
 There are six ways to play the game:
 
-1) ./incavead putty ascii
-2) ./incavead putty unicode
-3) ./incavead putty tiles
+1) ./incavead built-in client ascii
+2) ./incavead built-in client unicode
+3) ./incavead built-in client tiles
 4) ./incavead telnet ascii
 5) ./incavead telnet unicode
 6) ./incavead telnet tiles
 
-Ask for 'putty' if you want the Putty client, 
+Ask for 'tiletel' if you want the bundled built-in client. (Built with SDL, so it requires X.)
 'telnet' for the regular console telnet client.
 
 'ascii' is the simplest pure-ASCII graphics mode, 
 'unicode' enables Unicode graphics, and 
 'tiles' is the special 'square tiles' Unicode graphics mode.
 
-By default the game will launch the console telnet client and Unicode graphics.
+By default the game will launch the built-in client with square-tile graphics.
 
-
-(Putty is sometimes faster and more configurable as a telnet client, but may require some fiddling with settings.)
-
-Warning: if you want to use Putty, please install it first if you haven't yet. 
-
-For Ubuntu/Debian:
-
-  $ sudo apt-get install putty
-
-For RedHat/Fedora/OpenSUSE:
-
-  $ sudo yum install putty
-
-For Arch:
-
-  $ sudo pacman -S putty
-
-
-(Note: the game does not require Internet access. Everything is purely offline, telnet/Putty is used only as a convenient text-mode rendering client.)
+(Note: the game does not require Internet access. Everything is purely offline, telnet is used only as a convenient text-mode rendering client.)
 EOF
 
 mv dist incavead
-tar -cvzf incavead.tar.gz incavead
+tar -cvzf incavead-$ARCH.tar.gz incavead
 
--- platform/windows/makedist.sh
+++ platform/windows/makedist.sh
@@ -4,6 +4,7 @@ BUILDDIR=$1
 DATADIR=$2
 MINGW=/usr/i686-w64-mingw32/
 
+rm -rf incavead*
 
 mkdir dist
 mkdir dist/bin
@@ -11,35 +12,34 @@ cp $BUILDDIR/incavead.exe $BUILDDIR/highscore.exe $BUILDDIR/victory.exe $BUILDDI
 
 $MINGW/bin/objdump -x dist/bin/incavead.exe | grep 'DLL Name: ' | grep -o ': .*' | grep -o '[^: ]*' | xargs -IX find $MINGW -name X | xargs -IX cp X dist/bin
 
+$MINGW/bin/strip dist/bin/incavead.exe
+
 cp $DATADIR/*.cfg dist
 
 cat << "EOF" > dist/incavead_ascii.bat
 @ECHO OFF
 start "" /b bin\incavead.exe --singleplayer
-start putty -load game telnet://127.0.0.1:20020
+cd tiletel
+start tiletel localhost 20020
 EOF
 
 cat << "EOF" > dist/incavead_unicode.bat
 @ECHO OFF
 start "" /b bin\incavead.exe --unicode --singleplayer
-start putty -load game telnet://127.0.0.1:20020
+cd tiletel
+start tiletel localhost 20020
 EOF
 
 cat << "EOF" > dist/incavead_tiles.bat
 @ECHO OFF
 start "" /b bin\incavead.exe --tiles --singleplayer
-start putty -load game telnet://127.0.0.1:20020
+cd tiletel
+start tiletel localhost 20020
 EOF
 
-cp dist/incavead_unicode.bat dist/incavead.bat
+cp dist/incavead_tiles.bat dist/incavead.bat
 
-#wget -Pdist 'http://the.earth.li/~sgtatham/putty/latest/x86/puttytel.exe'
-
-# http://jakub.kotrla.net/putty/
-cp putty.exe dist
-cp putty.conf dist
-mkdir -p dist/putty/sessions
-cp game.session dist/putty/sessions
+unzip -d dist tiletel.zip
 
 cat << "EOF" > dist/README.txt
 
@@ -47,9 +47,9 @@ Run 'incavead_ascii.bat' to launch the game with ASCII graphics.
 Run 'incavead_unicode.bat' to launch the game with Unicode graphics.
 Run 'incavead_tiles.bat' to launch the game with 'square tiles' graphics.
 
-  P.S. 'incavead.bat' is the same thing as 'incavead_unicode.bat'.
+  P.S. 'incavead.bat' is the same thing as 'incavead_tiles.bat'.
 
-(Note: the game does not require Internet access. Everything is purely offline, Putty is used only as a convenient text-mode rendering client.)
+(Note: the game does not require Internet access. Everything is purely offline, telnet is used only as a convenient text-mode rendering client.)
 EOF
 
 mv dist incavead
