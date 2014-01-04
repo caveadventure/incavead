8e6d751ad0500de934731024cfa2f530301777a1
tkatchev <tkatchev@gmail.com>
2014-01-04 13:36:12 +0400

    .

--- spectator.h
+++ spectator.h
@@ -8,6 +8,192 @@
 
 namespace spectator {
 
+
+struct lo_fi_rw_mutex {
+
+    std::mutex mutex;
+    size_t reading;
+
+    lo_fi_rw_mutex() : reading(0) {}
+
+    struct r_lock {
+        lo_fi_rw_mutex& self;
+
+        r_lock(lo_fi_rw_mutex& s) : self(s) {
+            self.mutex.lock();
+            self.reading++;
+            self.mutex.unlock();
+        }
+
+        ~r_lock() {
+            self.mutex.lock();
+            self.reading--;
+            self.mutex.unlock();
+        }
+    };
+
+    struct w_lock {
+        lo_fi_rw_mutex& self;
+
+        w_lock(lo_fi_rw_mutex& s) : self(s) {
+
+            while (1) {
+                self.mutex.lock();
+
+                if (self.reading > 0) {
+                    self.mutex.unlock();
+                    continue;
+                }
+                
+                break;
+            }
+        }
+
+        ~w_lock() {
+            self.mutex.unlock();
+        }
+    };
+};
+
+
+template <typename SCREEN>
+bool copy_screen(const std::vector<maudit::glyph>& data, unsigned int ow, unsigned int oh, SCREEN& target);
+
+template <typename SCREEN>
+struct Screens {
+
+    struct info_t {
+        std::string name;
+        time_t ts;
+
+        std::vector<SCREEN*> links;
+
+        info_t() : ts(0) {}
+    };
+
+    std::map<SCREEN*, info_t> screens;
+
+    //std::mutex mutex;
+    lo_fi_rw_mutex mutex;
+
+    void add(SCREEN& s, const std::string& name) {
+
+        //std::unique_lock<std::mutex> l(mutex);
+        lo_fi_rw_mutex::w_lock l(mutex);
+
+        auto& tmp = screens[&s];
+        tmp.name = name;
+        tmp.ts = ::time(NULL);
+    }
+
+    struct list_entry_t {
+        void* tag;
+        std::string name;
+    };
+
+    std::map< time_t, std::vector<list_entry_t> > list() {
+
+        std::map< time_t, std::vector<list_entry_t> > ret;
+
+        //std::unique_lock<std::mutex> l(mutex);
+        lo_fi_rw_mutex::w_lock l(mutex);
+
+        for (const auto& i : screens) {
+            ret[i.second.ts].push_back(list_entry_t{(void*)i.first, i.second.name});
+        }
+
+        return ret;
+    }
+
+    void remove(SCREEN& s) {
+
+        //std::unique_lock<std::mutex> l(mutex);
+        lo_fi_rw_mutex::w_lock l(mutex);
+
+        screens.erase(&s);
+    }
+
+    bool link(void* tag, SCREEN& another) {
+
+        //std::unique_lock<std::mutex> l(mutex)
+        lo_fi_rw_mutex::w_lock l(mutex);
+
+        auto i = screens.find((SCREEN*)tag);
+
+        if (i == screens.end())
+            return false;
+
+        i->second.links.push_back(&another);
+
+        i->first->callback = std::bind(&Screens<SCREEN>::watching_callback, 
+                                       this, std::placeholders::_1, std::placeholders::_2);
+
+        return true;
+    }
+
+    void unlink(void* tag, SCREEN& another) {
+
+        //std::unique_lock<std::mutex> l(mutex);
+        lo_fi_rw_mutex::w_lock l(mutex);
+
+        auto i = screens.find((SCREEN*)tag);
+
+        if (i == screens.end())
+            return;
+
+        auto& links = i->second.links;
+
+        auto j = links.begin();
+        while (j != links.end()) {
+
+            if (*j == &another) {
+                j = links.erase(j);
+            } else {
+                ++j;
+            }
+        }
+
+        if (links.empty()) {
+            i->first->callback = nullptr;
+        }
+    }
+
+
+    void watching_callback(SCREEN* parent, const std::vector<maudit::glyph>& data) {
+
+        // TODO: This really needs read-write locks.
+
+        //std::unique_lock<std::mutex> l(mutex);
+        lo_fi_rw_mutex::r_lock l(mutex);
+
+        auto i = screens.find(parent);
+
+        if (i == screens.end())
+            return;
+
+        auto& links = i->second.links;
+
+        auto li = links.begin();
+        while (li != links.end()) {
+            auto& l = **li;
+
+            if (!copy_screen(data, parent->w, parent->h, l)) {
+                li = links.erase(li);
+            } else {
+                ++li;
+            }
+        }
+    }
+
+};
+
+template <typename SCREEN>
+Screens<SCREEN>& screens() {
+    static Screens<SCREEN> ret;
+    return ret;
+}
+
+
 template <typename SCREEN>
 bool copy_screen(const std::vector<maudit::glyph>& data, unsigned int ow, unsigned int oh, SCREEN& target) {
 
@@ -38,134 +224,6 @@ bool copy_screen(const std::vector<maudit::glyph>& data, unsigned int ow, unsign
     }
 
     return target.send_screen(temp);
-}
-
-
-template <typename SCREEN>
-struct Screens {
-
-    struct info_t {
-        std::string name;
-        time_t ts;
-
-        std::vector<SCREEN*> links;
-
-        info_t() : ts(0) {}
-    };
-
-    std::map<SCREEN*, info_t> screens;
-
-    std::mutex mutex;
-
-    void add(SCREEN& s, const std::string& name) {
-
-        std::unique_lock<std::mutex> l(mutex);
-
-        auto& tmp = screens[&s];
-        tmp.name = name;
-        tmp.ts = ::time(NULL);
-    }
-
-    struct list_entry_t {
-        void* tag;
-        std::string name;
-    };
-
-    std::map< time_t, std::vector<list_entry_t> > list() {
-
-        std::map< time_t, std::vector<list_entry_t> > ret;
-
-        std::unique_lock<std::mutex> l(mutex);
-
-        for (const auto& i : screens) {
-            ret[i.second.ts].push_back(list_entry_t{(void*)i.first, i.second.name});
-        }
-
-        return ret;
-    }
-
-    void remove(SCREEN& s) {
-
-        std::unique_lock<std::mutex> l(mutex);
-    
-        screens.erase(&s);
-    }
-
-    bool link(void* tag, SCREEN& another) {
-
-        std::unique_lock<std::mutex> l(mutex);
-
-        auto i = screens.find((SCREEN*)tag);
-
-        if (i == screens.end())
-            return false;
-
-        i->second.links.push_back(&another);
-
-        i->first->callback = std::bind(&Screens<SCREEN>::watching_callback, 
-                                       this, std::placeholders::_1, std::placeholders::_2);
-
-        return true;
-    }
-
-    void unlink(void* tag, SCREEN& another) {
-
-        std::unique_lock<std::mutex> l(mutex);
-
-        auto i = screens.find((SCREEN*)tag);
-
-        if (i == screens.end())
-            return;
-
-        auto& links = i->second.links;
-
-        auto j = links.begin();
-        while (j != links.end()) {
-
-            if (*j == &another) {
-                j = links.erase(j);
-            } else {
-                ++j;
-            }
-        }
-
-        if (links.empty()) {
-            i->first->callback = nullptr;
-        }
-    }
-
-
-    void watching_callback(SCREEN* parent, const std::vector<maudit::glyph>& data) {
-
-        // TODO: This really needs read-write locks.
-
-        std::unique_lock<std::mutex> l(mutex);
-
-        auto i = screens.find(parent);
-
-        if (i == screens.end())
-            return;
-
-        auto& links = i->second.links;
-
-        auto li = links.begin();
-        while (li != links.end()) {
-            auto& l = **li;
-
-            if (!copy_screen(data, parent->w, parent->h, l)) {
-                li = links.erase(li);
-            } else {
-                ++li;
-            }
-        }
-    }
-
-};
-
-template <typename SCREEN>
-Screens<SCREEN>& screens() {
-    static Screens<SCREEN> ret;
-    return ret;
 }
 
 
