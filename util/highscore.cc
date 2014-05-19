bfe60aa9450d61c23717616f08f24116c068f7d1
tkatchev <tkatchev@gmail.com>
2014-05-19 10:50:02 +0400

    .

--- util/highscore.cc
+++ util/highscore.cc
@@ -47,6 +47,7 @@ struct other_stats_t {
 
     double gdp1;
     double gdp2;
+    double gdp3;
 
     double avg_plev;
     double avg_dlev;
@@ -59,9 +60,9 @@ struct other_stats_t {
 
     size_t ngames;
 
-    std::pair<std::string, double> got_rich_quick;
+    std::pair< std::string, std::pair<double, std::string> > got_rich_quick;
 
-    other_stats_t() : gdp1(0), gdp2(0), avg_plev(0), avg_dlev(0), 
+    other_stats_t() : gdp1(0), gdp2(0), gdp3(0), avg_plev(0), avg_dlev(0), 
                       median_plev(0), median_dlev(0), players(0), ngames(0) {}
 
     void operator()(const std::vector<highscore::Scores::order_t>& v) {
@@ -76,6 +77,8 @@ struct other_stats_t {
 
         typedef std::map< std::string, std::pair<unsigned int, size_t> > by_players_t;
         by_players_t by_players;
+
+        std::map<int, double> gdp_lev;
 
         for (const auto& i : v) {
 
@@ -94,9 +97,18 @@ struct other_stats_t {
             tmb.first++;
             tmb.second = std::max(tmb.second, i.scum_streak);
 
-            if (i.plev == 0 && i.worth >= got_rich_quick.second) {
+            if (i.plev == 0 && i.worth >= got_rich_quick.second.first) {
                 got_rich_quick.first = i.bone.name.name;
-                got_rich_quick.second = i.worth;
+                got_rich_quick.second.first = i.worth;
+                got_rich_quick.second.second = i.bone.cause.name;
+            }
+
+            gdp_lev[i.dlev] += i.bone.worth;
+        }
+
+        for (const auto& i : gdp_lev) {
+            if (i.second > 0) {
+                gdp3 += i.second;
             }
         }
 
@@ -163,31 +175,32 @@ struct other_stats_t {
         most_active_player.first = quote(most_active_player.first);
         scummer.first = quote(scummer.first);
         got_rich_quick.first = quote(got_rich_quick.first);
+        got_rich_quick.second.second = quote(got_rich_quick.second.second);
 
         std::cout << nlp::message("\n{\n"
                                   "\"games\": %d,\n"
                                   "\"kills_raw\": { \"cause\": \"%s\", \"kills\": %d },\n"
                                   "\"kills_plev\": { \"cause\": \"%s\", \"kills\": %d },\n"
                                   "\"kills_worth\": { \"cause\": \"%s\", \"kills\": %d },\n"
-                                  "\"gdp\": { \"a\": %d, \"b\": %d },\n"
+                                  "\"gdp\": { \"a\": %d, \"b\": %d, \"c\": %d },\n"
                                   "\"plev\": { \"avg\": %d, \"median\": %d },\n"
                                   "\"dlev\": { \"avg\": %d, \"median\": %d },\n"
                                   "\"players\": %d,\n"
                                   "\"most_active\": { \"name\": \"%S\", \"games\": %d },\n"
                                   "\"scummer\": { \"name\": \"%S\", \"streak\": %d },\n"
-                                  "\"got_rich_quick\": { \"name\": \"%S\", \"worth\": %d }\n"
+                                  "\"got_rich_quick\": { \"name\": \"%S\", \"worth\": %d, \"cause\": \"%s\" }\n"
                                   "}\n",
                                   ngames,
                                   cause_raw.first, cause_raw.second, 
                                   cause_plev.first, cause_plev.second,
                                   cause_worth.first, cause_worth.second,
-                                  gdp1, gdp2,
+                                  gdp1, gdp2, gdp3,
                                   avg_plev+1, median_plev+1,
                                   avg_dlev, median_dlev,
                                   players,
                                   most_active_player.first, most_active_player.second,
                                   scummer.first, scummer.second,
-                                  got_rich_quick.first, got_rich_quick.second);
+                                  got_rich_quick.first, got_rich_quick.second.first, got_rich_quick.second.second);
     }
 };
 
