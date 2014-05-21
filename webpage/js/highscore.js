2d9b091a3a2bdfb98f644af2eeb8a28ee2461e6c
tkatchev <tkatchev@gmail.com>
2014-05-21 23:44:14 +0400

    .

--- webpage/js/highscore.js
+++ webpage/js/highscore.js
@@ -40,57 +40,6 @@ function format_scoreline(item) {
     return l;
 }
 
-function format_ringstatus(data) {
-
-    if (data["locations"].length == 0) {
-        var ts  = data["ts"]*1000;
-        var now = (new Date().getTime());
-        var dif = now - ts;
-
-        if (ts == 0 || dif >= 7200000) {
-            return "Ready to regenerate on dungeon level 1.";
-        } else {
-            return "Destroyed or in possession since " + (new Date(ts).toLocaleString()) + 
-                ".<br/>(" + Math.round((7200000 - dif)/60000) + " minutes left until regeneration.)";
-        }
-    } else {
-        var loc = data["locations"][0];
-        var off = loc["dlev_offset"];
-        var tab = [
-            [ "follow a tunnel due north-west",
-              "follow a tunnel due west", 
-              "follow a tunnel due south-west" ],
-            [ "follow a tunnel due north",
-              "main branch", 
-              "follow a tunnel due south" ],
-            [ "follow a tunnel due north-east",
-              "follow a tunnel due east", 
-              "follow a tunnel due south-east" ] ];
-        return "Found somewhere on dungeon level " + (loc["dlev"] + 1) + " (" + 
-            tab[off[0]+1][off[1]+1] +
-            ").<br/>It has been there since " + (new Date(data["ts"]*1000).toLocaleString()) + ".";
-    }
-}
-
-
-function format_gamestats(data) {
-    return "<p>" +
-        "Leading cause of death: <b>" + data.kills_raw.cause + "</b>, with <b>" + data.kills_raw.kills + "</b> kills.<br/>" +
-        "Leading cause of death, weighed for player level: <b>" + data.kills_plev.cause + "</b>, with <b>" + data.kills_plev.kills + "</b> kills.<br/>" +
-        "Leading cause of death, weighed for player net worth: <b>" + data.kills_worth.cause + "</b>, with <b>" + data.kills_worth.kills + "</b> kills.<br/>" +
-        "World GDP: <b>" + data.gdp.c + "</b> $ZM.<br/>" + 
-        "Average player level: <b>" + data.plev.avg + "</b><br/>" +
-        "Median player level: <b>" + data.plev.median + "</b><br/>" +
-        "Average dungeon level at time of death: <b>" + data.dlev.avg + "</b><br/>" +
-        "Median dungeon level at time of death: <b>" + data.dlev.median + "</b><br/>" +
-        "Number of unique player names: <b>" + data.players + "</b><br/>" +
-        "Most active player: <b>" + data.most_active.name + "</b>, with <b>" + data.most_active.games + "</b> games played.<br/>" +
-        "Most active replay code user: <b>" + data.scummer.name + "</b>, who used a replay code <b>" + data.scummer.streak + "</b> times.<br/>" +
-        "Most achievements achieved: <b>" + data.most_achievements.name + "</b> with <b>" + data.most_achievements.number + "</b><br/>" +
-        "And the <em>Get Rich Quick Award</em> goes to <b>" + data.got_rich_quick.name + "</b>, who got rich quick with <b>" + 
-        data.got_rich_quick.worth + "</b> $ZM and was killed by <b>" + data.got_rich_quick.cause + "</b>" +
-        "</p>";
-}
 
 function do_highscore() {
 
@@ -103,29 +52,15 @@ function do_highscore() {
     $.getJSON(jsonfile, {}, 
               function(data) {
 
-                  $.each(data["highscores"]["plev"], function(i,item) {
-                      $("<li/>").html(format_scoreline(item)).appendTo("#highscore_plev")
-                  });
+                  $("#panel_plev").append(ich.plev_tmpl(data.highscores));
+                  $("#panel_dlev_d").append(ich.dlev_d_tmpl(data.highscores));
+                  $("#panel_dlev_a").append(ich.dlev_a_tmpl(data.highscores));
+                  $("#panel_worth").append(ich.worth_tmpl(data.highscores));
+                  $("#panel_ts").append(ich.ts_tmpl(data.highscores));
 
-                  $.each(data["highscores"]["dlev_d"], function(i,item) {
-                      $("<li/>").html(format_scoreline(item)).appendTo("#highscore_dlev_d")
-                  });
+                  $("#num_games").append(ich.num_games_tmpl(data));
 
-                  $.each(data["highscores"]["dlev_a"], function(i,item) {
-                      $("<li/>").html(format_scoreline(item)).appendTo("#highscore_dlev_a")
-                  });
-
-                  $.each(data["highscores"]["worth"], function(i,item) {
-                      $("<li/>").html(format_scoreline(item)).appendTo("#highscore_worth")
-                  });
-
-                  $.each(data["highscores"]["ts"], function(i,item) {
-                      $("<li/>").html(format_scoreline(item)).appendTo("#highscore_ts")
-                  });
-
-                  $("<span/>").html(data["num_games"]).appendTo("#num_games");
-
-                  $("<p/>").html(format_gamestats(data["stats"])).appendTo("#panel_stats");
+                  $("#panel_stats").append(ich.stats_tmpl(data.stats));
               }
              );
 
@@ -142,7 +77,48 @@ function do_ringstatus() {
 
     $.getJSON(jsonfile, {},
               function(data) {
-                  $("<span/>").html(format_ringstatus(data)).appendTo("#ringstatus");
+
+                  var ts = data.ts*1000;
+                  var now = (new Date().getTime());
+                  var ts_loc = (new Date(ts).toLocaleString());
+                  var dif = now - ts;
+                  var minutes = Math.round((7200000 - dif)/60000);
+                  var regenerate = false;
+                  var destroyed = false;
+                  var found = false;
+                  var dlev;
+                  var branch;
+
+                  if (data.locations.length == 0) {
+                      if (ts == 0 || dif >= 7200000) {
+                          regenerate = true;
+                      } else {
+                          destroyed = true;
+                      }
+                  } else {
+
+                      var loc = data.locations[0];
+                      var off = loc.dlev_offset;
+                      var tab = [
+                          [ "follow a tunnel due north-west",
+                            "follow a tunnel due west", 
+                            "follow a tunnel due south-west" ],
+                          [ "follow a tunnel due north",
+                            "main branch", 
+                            "follow a tunnel due south" ],
+                          [ "follow a tunnel due north-east",
+                            "follow a tunnel due east", 
+                            "follow a tunnel due south-east" ] ];
+
+                      dlev = loc.dlev + 1;
+                      branch = tab[off[0]+1][off[1]+1];
+                      found = true;
+                  }
+
+                  var obj = { 'regenerate':regenerate, 'destroyed':destroyed, 'found':found,
+                              'ts':ts_loc, 'minutes':minutes, 'dlev':dlev, 'branch':branch };
+
+                  $("#ringstatus").append(ich.ringstatus_tmpl(obj));
               }
              );
 }
