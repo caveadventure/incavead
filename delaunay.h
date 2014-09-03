6070d6c784f21bc864dbc330d019d8758b0ffc61
tkatchev <tkatchev@gmail.com>
2014-09-03 19:44:00 +0400

    .

--- delaunay.h
+++ delaunay.h
@@ -6,44 +6,53 @@
 #include <map>
 #include <algorithm>
 #include <utility>
+#include <memory>
+#include <array>
 
 #include <math.h>
 
 namespace delaunay {
 
-struct pt {
-    int x;
-    int y;
+template <typename T>
+struct point {
+    T x;
+    T y;
 
-    pt(int _x = 0, int _y = 0) : x(_x), y(_y) {}
+    point(T _x = 0, T _y = 0) : x(_x), y(_y) {}
 
-    pt(const std::pair<unsigned int, unsigned int>& _pt) : x(_pt.first), y(_pt.second) {}
+    template <typename S>
+    point(const point<S>& a) : x(a.x), y(a.y) {}
 
-    pt operator+(const pt& a) const {
-        return pt(x + a.x, y + a.y);
+    template <typename S>
+    point(const std::pair<S, S>& _pt) : x(_pt.first), y(_pt.second) {}
+
+    template <typename S>
+    point<T> operator+(const point<S>& a) const {
+        return point<T>(x + a.x, y + a.y);
     }
 
-    pt operator-(const pt& a) const {
-        return pt(x - a.x, y - a.y);
+    template <typename S>
+    point<T> operator-(const point<S>& a) const {
+        return point<T>(x - a.x, y - a.y);
     }
 
     template <typename POD>
-    pt operator*(POD d) const {
-        return pt(x * d, y * d);
+    point<T> operator*(POD d) const {
+        return point<T>(x * d, y * d);
     }
 
     template <typename POD>
-    pt operator/(POD d) const {
-        return pt(x / d, y / d);
+    point<T> operator/(POD d) const {
+        return point<T>(x / d, y / d);
     }
 
-    bool operator<(const pt& a) const {
+    bool operator<(const point<T>& a) const {
         if (x < a.x) return true;
         if (x == a.x && y < a.y) return true;
         return false;
     }
 
-    bool operator==(const pt& a) const {
+    bool operator==(const point<T>& a) const {
         return (x == a.x && y == a.y);
     }
 
@@ -51,8 +60,58 @@ struct pt {
         return x*x + y*y;
     }
 
-    unsigned int dist2(const pt& a) const {
+    template <typename S>
+    unsigned int dist2(const point<S>& a) const {
         return (*this - a).dist2();
+    }
+
+    double dist() const {
+        return ::sqrt(dist2());
+    }
+
+    template <typename S>
+    double dist(const point<S>& a) const {
+        return ::sqrt(dist2(a));
+    }
+};
+
+typedef point<int> pt;
+
+struct circle {
+
+    point<double> center;
+    double r2;
+    double r;
+
+    circle() : r2(0), r(0) {}
+
+    template <typename S>
+    circle(const point<S>& _c, double _r2) : center(_c), r2(_r2), r(::sqrt(r)) {}
+
+    template <typename S>
+    circle(const point<S>& _c, double _r, bool) : center(_c), r2(_r*_r), r(_r) {}
+
+    bool null() const {
+        return (r2 == 0);
+    }
+
+    template <typename T>
+    bool has(const point<T>& p) const {
+        return (center.dist2(p) <= r2);
+    }
+
+    bool has(const circle& c) const {
+        return (center.dist(c.center) + c.r <= r);
+    }
+
+    bool intersects(const circle& c) const {
+        return (center.dist(c.center) - c.r <= r);
+    }
+
+    bool operator<(const circle& c) const {
+        if (center < c.center) return true;
+        if (center == c.center && r2 < c.r2) return true;
+        return false;
     }
 };
 
@@ -62,12 +121,11 @@ struct tri {
     pt b;
     pt c;
 
-    pt center;
-    double radius;
+    circle circ;
 
-    tri() : radius(0) {}
+    tri() {}
 
-    tri(const pt& _a, const pt& _b, const pt& _c) : a(_a), b(_b), c(_c), radius(0) { circumscribe(); }
+    tri(const pt& _a, const pt& _b, const pt& _c) : a(_a), b(_b), c(_c) { circumscribe(); }
 
     void circumscribe() {
 
@@ -83,25 +141,28 @@ struct tri {
         double q = (a.x * b_c.y + b.x * c_a.y + c.x * a_b.y);
 
         if (::fabs(q) < 0.00001) {
-            radius = 0;
+
+            circ = circle(pt(0, 0), 0);
             return;
         }
 
         double D = 0.5 / q;
 
-        center = pt((da * b_c.y + db * c_a.y + dc * a_b.y),
-                   -(da * b_c.x + db * c_a.x + dc * a_b.x)) * D;
+        pt center = pt((da * b_c.y + db * c_a.y + dc * a_b.y),
+                       -(da * b_c.x + db * c_a.x + dc * a_b.x)) * D;
 
-        radius = (a - center).dist2();
+        double radius = center.dist2(a);
+
+        circ = circle(center, radius);
     }
 
     bool operator<(const tri& a) const {
-        return center < a.center;
+        return circ.center < a.circ.center;
     }
 
     bool null() const {
 
-        return (radius == 0);
+        return circ.null(); 
     }
 };
 
@@ -131,6 +192,92 @@ struct edge {
     }
 };
 
+
+struct flower_t;
+typedef std::unique_ptr<flower_t> flower;
+
+struct flower_t {
+
+    std::set<tri> data;
+    std::array<flower, 7> bud;
+
+    static circle petal(const circle& super, int i) {
+
+        static double sqrt3_2 = 0.8660254037844386;
+
+        static double centers[7][2] = {
+            { 0,             0 },
+            { sqrt3_2,       0 },
+            { sqrt3_2 / 2,   0.75 },
+            { sqrt3_2 / 2,   -0.75 },
+            { -sqrt3_2,      0 },
+            { -sqrt3_2 / 2,  0.75 },
+            { -sqrt3_2 / 2,  -0.75 }
+        };
+
+        return circle(point<double>(super.center.x + centers[i][0] * super.r,
+                                    super.center.y + centers[i][1] * super.r), 
+                      super.r / 2, true);
+    }
+
+    static bool insert(flower& root, const circle& super, const tri& t) {
+
+        if (!root) 
+            root = flower(new flower_t);
+
+        for (int i = 0; i < 7; ++i) {
+
+            flower& f = root->bud[i];
+
+            circle fc = petal(super, i);
+
+            if (fc.has(t.circ))
+                return insert(f, fc, t);
+        }
+
+        root->data.insert(t);
+        return true;
+    }
+
+    static void query(const flower& root, const circle& super, const pt& q, std::set<tri>& ret) {
+
+        if (!root)
+            return;
+
+        for (int i = 0; i < 7; ++i) {
+
+            const flower& f = root->bud[i];
+
+            circle fc = petal(super, i);
+
+            if (fc.has(q))
+                query(f, fc, q, ret);
+        }
+
+        for (const tri& i : root->data) {
+            if (i.circ.has(q)) {
+                ret.insert(i);
+            }
+        }
+    }
+
+    static void result(const flower& root, std::set<tri>& ret) {
+
+        if (!root)
+            return;
+
+        for (int i = 0; i < 7; ++i) {
+
+            const flower& f = root->bud[i];
+
+            result(f, ret);
+        }
+
+        ret.insert(root->data.begin(), root->data.end());
+    }
+};
+
+
 struct Triangulation {
 
     std::map< pt, std::set<pt> > res;
@@ -152,81 +299,39 @@ struct Triangulation {
         if (points.empty())
             return;
 
-        std::multiset<tri> queue;
+        flower tree(new flower_t);
 
-        auto pi = points.begin();
+        pt fake1(0, 0);
+        pt fake2(w-1, 0);
+        pt fake3(w-1, h-1);
+        pt fake4(0, h-1);
 
-        {
-            pt fake1(0, 0);
-            pt fake2(w-1, 0);
-            pt fake3(w-1, h-1);
-            pt fake4(0, h-1);
+        circle super(point<double>(w / 2, h / 2), fake3.dist2() + 1);
 
-            pt p = *pi;
- 
-            tri top(fake1, fake2, p);
-            tri rig(fake2, fake3, p);
-            tri bot(fake3, fake4, p);
-            tri lef(fake4, fake1, p);
+        tri top(fake1, fake2, fake3);
+        tri bot(fake1, fake4, fake3);
 
-            if (!top.null()) queue.insert(top);
-            if (!rig.null()) queue.insert(rig);
-            if (!bot.null()) queue.insert(bot);
-            if (!lef.null()) queue.insert(lef);
+        flower_t::insert(tree, super, top);
+        flower_t::insert(tree, super, bot);
 
-            if (points.count(fake1) == 0) fakes.insert(fake1);
-            if (points.count(fake2) == 0) fakes.insert(fake2);
-            if (points.count(fake3) == 0) fakes.insert(fake3);
-            if (points.count(fake4) == 0) fakes.insert(fake4);
-        }
+        if (points.count(fake1) == 0) fakes.insert(fake1);
+        if (points.count(fake2) == 0) fakes.insert(fake2);
+        if (points.count(fake3) == 0) fakes.insert(fake3);
+        if (points.count(fake4) == 0) fakes.insert(fake4);
 
-        ++pi;
-        while (pi != points.end()) {
-
-            const pt& p = *pi;
+        for (const pt& p : points) {
 
             std::map<edge, size_t> loose_edges;
 
-            for (auto i = queue.begin(); i != queue.end();) {
+            std::set<tri> bad;
 
-                const tri& t = *i;
+            flower_t::query(tree, super, p, bad);
 
-                if (p.x > t.center.x + ::sqrt(t.radius)) {
+            for (const tri& t : bad) {
 
-                    // The triangle is too far away from the inserted point and so will not change.
-
-                    auto& ta = res[t.a];
-                    auto& tb = res[t.b];
-                    auto& tc = res[t.c];
-
-                    std::cout << t.a.x << " " << t.a.y << std::endl;
-                    std::cout << t.b.x << " " << t.b.y << std::endl;
-                    std::cout << t.c.x << " " << t.c.y << std::endl;
-                    std::cout << t.a.x << " " << t.a.y << std::endl << std::endl;
-
-                    ta.insert(t.b);
-                    ta.insert(t.c);
-                    tb.insert(t.a);
-                    tb.insert(t.c);
-                    tc.insert(t.a);
-                    tc.insert(t.b);
-
-                    i = queue.erase(i);
-
-                } else if ((p - t.center).dist2() <= t.radius) {
-
-                    // The inserted point intersects the triangle.
-
-                    loose_edges[edge(t.a, t.b)]++;
-                    loose_edges[edge(t.b, t.c)]++;
-                    loose_edges[edge(t.c, t.a)]++;
-
-                    i = queue.erase(i);
-
-                } else {
-
-                    ++i;
-                }
+                loose_edges[edge(t.a, t.b)]++;
+                loose_edges[edge(t.b, t.c)]++;
+                loose_edges[edge(t.c, t.a)]++;
             }
 
             for (const auto& v : loose_edges) {
@@ -239,13 +344,15 @@ struct Triangulation {
                 tri tmp(v.first.a, v.first.b, p);
 
                 if (!tmp.null())    
-                    queue.insert(tmp);
+                    flower_t::insert(tree, super, tmp);
             }
-
-            ++pi;
         }
 
-        for (const auto& t : queue) {
+        std::set<tri> finaltri;
+
+        flower_t::result(tree, finaltri);
+
+        for (const auto& t : finaltri) {
 
             auto& ta = res[t.a];
             auto& tb = res[t.b];
@@ -296,7 +403,7 @@ struct Triangulation {
             if (fakes.count(p) != 0)
                 continue;
 
-            ret.insert(neighbor_t(::sqrt((p0 - p).dist2()), p.x, p.y));
+            ret.insert(neighbor_t(p0.dist(p), p.x, p.y));
         }
 
         return ret;
